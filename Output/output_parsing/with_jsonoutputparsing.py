b684740f0f9478e4bdd942a450f415288d17644e
the-sahilsuman <sahilsuman8905@gmail.com>
2026-08-09 09:53:09 +0530

    parsing str and json

---
+++ Output/output_parsing/with_jsonoutputparsing.py
@@ -0,0 +1,28 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+parser = JsonOutputParser()
+
+template = PromptTemplate(
+    template="Give the name, age, city of a fictional person \n {format_instruction}",
+    input_variables=[],
+    partial_variables={"format_instruction": parser.get_format_instructions()}
+)
+
+llm = HuggingFaceEndpoint(
+    repo_id=os.getenv("HUGGINGFACE_MODEL_REPO"),
+    task="text-generation",
+    max_new_tokens=300,
+    temperature=0.8
+)
+
+chat_model = ChatHuggingFace(llm=llm)
+
+chain = template | chat_model | parser
+result = chain.invoke({})
+print(result)
